7b1a46613f7ebc34b811e8dc15390d9bd6d32ede
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-04-11 17:31:13 +0530

    Code

---
+++ express3/routes/userroutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const route = express.Router();
+route.post('/register',(request, response)=>{
+    var userid = request.body.userid;
+    var pwd = request.body.pwd;
+    if(userid==pwd){
+        let path  = require("path");
+        let fullPath = path.normalize(__dirname+"/..");
+        fullPath = path.join(fullPath,"/public/pages/welcome.html");
+        //response.sendFile(fullPath);
+        var menus =[{'name':'Login','link':'login.html'},{'name':'Register','link':'register.html'},{'name':'Sales','link':'sales.html'}];
+        response.render('welcome',{"user":userid,'menus':menus});
+        //response.send("U Register SuccessFully....");
+    }
+    else{
+        response.send("Can't Register...");
+    }
+    });
+    
+    route.get('/login',(request, response)=>{
+        console.log("User ",request.query);
+        var userid = request.query.userid;
+        var pwd = request.query.pwd;
+        if(userid == pwd){
+            response.send('Welcome '+userid);
+        }
+        else{
+            response.send('Invalid userid or Password');
+        }
+        //response.json({id:1001,name:'Ram'});
+       // response.send('U Requested for Login ');
+    
+    });
+    module.exports = route;
+
+
+
+
+
+
